972df9eee726b0bfe491a798d606d3fe015e0e6b
Arg39 <pabannu@gmail.com>
2024-10-12 09:26:50 +0700

    main page update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:maker/main2.dart';
+import 'package:maker/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
